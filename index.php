6cf3dcef1f2b1be24af81ab2cc4d175f11161915
MyUsername <my@email.com>
2023-02-04 10:52:09 +0100

    create a class and require once

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+  require_once __DIR__ . '/models/Movie.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
    <head>
@@ -7,6 +11,8 @@
       <title>OOP</title>
    </head>
    <body>
-      
+   <?php 
+
+   ?>
    </body>
 </html>
